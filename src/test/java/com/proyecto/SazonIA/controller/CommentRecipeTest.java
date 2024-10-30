8159b92fc8ae07e1a97c75e5a80cb25a5836e12e
CandeGod <Candebeatriz14@gmail.com>
2024-10-30 16:36:38 -0600

    Delete imports

--- src/test/java/com/proyecto/SazonIA/controller/CommentRecipeTest.java
+++ src/test/java/com/proyecto/SazonIA/controller/CommentRecipeTest.java
@@ -1,14 +1,9 @@
 package com.proyecto.SazonIA.controller;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
--- src/test/java/com/proyecto/SazonIA/controller/RecipeControllerTest.java
+++ src/test/java/com/proyecto/SazonIA/controller/RecipeControllerTest.java
@@ -1,15 +1,12 @@
 package com.proyecto.SazonIA.controller;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +15,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import com.proyecto.SazonIA.model.CommentRecipe;
-import com.proyecto.SazonIA.model.Recipe;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
