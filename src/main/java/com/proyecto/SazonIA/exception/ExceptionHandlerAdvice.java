02b6af6d187208409816facc430ea949742bfc69
CandeGod <Candebeatriz14@gmail.com>
2024-11-08 15:38:58 -0600

    Remove an anottation

--- src/main/java/com/proyecto/SazonIA/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/proyecto/SazonIA/exception/ExceptionHandlerAdvice.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import com.google.gson.Gson;
 
