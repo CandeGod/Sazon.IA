0fe98457353a12855e193b6d2746ff541c411eee
AldahirM <l21te0275@teziutlan.tecnm.mx>
2024-11-21 09:01:32 -0600

    Add ModelMapper integration and update Recipe handling with DTOs

---
+++ src/main/java/com/proyecto/SazonIA/DTO/RecipeDTO.java
@@ -0,0 +1,78 @@
+package com.proyecto.SazonIA.DTO;
+
+public class RecipeDTO {
+
+    private Integer recipe_id;
+    private String recipe_name;
+    private String ingredients;
+    private String instructions;
+    private String preparation_time;
+    private String difficulty;
+    private String recipe_time_stamp;
+    private Integer user_id;
+
+    public Integer getRecipe_id() {
+        return recipe_id;
+    }
+
+    public void setRecipe_id(Integer recipe_id) {
+        this.recipe_id = recipe_id;
+    }
+
+    public String getRecipe_name() {
+        return recipe_name;
+    }
+
+    public void setRecipe_name(String recipe_name) {
+        this.recipe_name = recipe_name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getPreparation_time() {
+        return preparation_time;
+    }
+
+    public void setPreparation_time(String preparation_time) {
+        this.preparation_time = preparation_time;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getRecipe_time_stamp() {
+        return recipe_time_stamp;
+    }
+
+    public void setRecipe_time_stamp(String recipe_time_stamp) {
+        this.recipe_time_stamp = recipe_time_stamp;
+    }
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+}
